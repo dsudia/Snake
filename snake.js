b6171f075702870c5b357320ac75910fbe38a2fa
David Sudia <dsudia@gmail.com>
2016-01-20 11:32:32 -0700

    add drawSnake

--- snake.js
+++ snake.js
@@ -10,10 +10,22 @@ function drawBorder (color) {
   context.strokeRect(0,0, canvas.width, canvas.height);
 }
 
+function drawSnake () {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var pixelSize = canvas.width / 25;
+    for (i = 0; i < 8; i ++) {
+      context.fillStyle = 'orange';
+      context.fillRect(i.pixelSize, 0, pixelSize, pixelSize);
+      context.strokeStyle = 'white';
+      context.strokeRect(i*pixelSize, 0, pixelSize, pixelSize);
+    }
+}
+
 function Game() {
   var interval = function() {
     drawBorder('red');
-
+    drawSnake();
 
 
 
